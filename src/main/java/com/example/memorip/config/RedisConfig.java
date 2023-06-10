141d2335631d0ab8a47c9ca12ebfa0956e42ea43
ygreenb <nhj7911@gmail.com>
2023-06-11 01:55:20 +0900

    fix : redislabs 연결 추가

--- src/main/java/com/example/memorip/config/RedisConfig.java
+++ src/main/java/com/example/memorip/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -15,10 +17,17 @@ public class RedisConfig {
     @Value("${spring.data.redis.port}")
     private int port;
 
+    @Value("${spring.data.redis.password}") // Redis password 추가
+    private String password;
+
     @Bean // redis와 연결
-    public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(host, port);
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+        redisConfig.setPassword(RedisPassword.of(password));
+
+        return new LettuceConnectionFactory(redisConfig);
     }
+
 
     @Bean // RedisConnection을 통해 넘어온 byte 값을 객체 직렬화 해줌
     public RedisTemplate<?,?> redisTemplate(){
