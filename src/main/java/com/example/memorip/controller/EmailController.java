077b434ec2fd4917b53dcf5e8edd8f340d79cb30
ygreenb <nhj7911@gmail.com>
2023-06-12 00:32:47 +0900

    fix: 응답값 객체로 수정

--- src/main/java/com/example/memorip/controller/EmailController.java
+++ src/main/java/com/example/memorip/controller/EmailController.java
@@ -1,5 +1,6 @@
 package com.example.memorip.controller;
 
+import com.example.memorip.exception.DefaultRes;
 import com.example.memorip.service.EmailServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -31,21 +32,21 @@ public class EmailController {
             @RequestParam("rcv") String emailRcv) throws Exception {
         log.info("rcv: " + emailRcv);
         emailService.sendVerificationCodeEmail(emailRcv);
-        return ResponseEntity.ok(HttpStatus.OK);
+        return new ResponseEntity<>(DefaultRes.res(200, "이메일 전송 완료", ""), HttpStatus.OK);
     }
 
     @Operation(summary = "인증코드 확인", description = "이메일 인증코드가 맞는지 검증하는 메서드입니다.")
-
     @GetMapping("/verifyCode")
     @ResponseBody
     public ResponseEntity<?> verifyCode(
             @Parameter(description = "인증받을 이메일", required = true)
             @RequestParam("rcv") String emailRcv,
             @Parameter(description = "인증코드", required = true)
-            @RequestParam("code") String code) throws Exception {
+            @RequestParam("code") String code){
         log.info("rcv: " + emailRcv);
         log.info("code: " + code);
         return emailService.verifyEmailCode(emailRcv, code)?
-                ResponseEntity.ok(HttpStatus.OK) : ResponseEntity.badRequest().build();
+                new ResponseEntity<>(DefaultRes.res(200, "이메일 인증 성공", ""), HttpStatus.OK)
+                : new ResponseEntity<>(DefaultRes.res(400, "잘못된 인증코드입니다.", ""), HttpStatus.BAD_REQUEST);
     }
 }
