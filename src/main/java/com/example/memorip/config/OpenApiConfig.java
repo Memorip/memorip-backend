f04f5d476f4b0aa70498b117e4e1be388a5c4725
HyungWookChoi <qpflapffhs76@gmail.com>
2023-06-12 00:20:45 +0900

    feat: Swagger Bearer JWT 추가

--- src/main/java/com/example/memorip/config/OpenApiConfig.java
+++ src/main/java/com/example/memorip/config/OpenApiConfig.java
@@ -3,6 +3,8 @@ package com.example.memorip.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,8 +16,22 @@ public class OpenApiConfig {
                 .title("Memorip API Document")
                 .version("v1.0.0")
                 .description("Memorip의 API 명세서입니다.");
+
+
+        String jwtSchemeName = "Authorization";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components().addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                );
+
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+
     }
 }
